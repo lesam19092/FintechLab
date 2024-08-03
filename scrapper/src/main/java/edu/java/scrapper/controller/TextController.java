8f37d82ac943fa9fc6b095dd417d237550496fdd
lesam19092 <danigpro1337@gmail.com>
2024-08-03 13:19:33 +0300

    переделал фронт на тимлив

--- scrapper/src/main/java/edu/java/scrapper/controller/TextController.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/TextController.java
@@ -2,13 +2,33 @@ package edu.java.scrapper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TextController {
 
-    @RequestMapping("/text")
-    public String processText(Model model) {
-        return "main.html";
+    /*@RequestMapping("/text")
+    public String translateText(
+        Model model
+    ) {
+        {
+            System.out.println(model.toString());
+            return "main.html";
+        }
+    }*/
+
+    @RequestMapping("/")
+    public String home(Model m) {
+        return "main";
+    }
+
+    @GetMapping("/translated")
+    public String add(@RequestParam("textForTranslation") String text, Model model) {
+        System.out.println(text); // Проверка, что значение текста корректно получено
+        model.addAttribute("translatedText", text); // Добавляем значение в модель под ключом "message"
+        return "main"; // Возвращаем имя шаблона без расширения (.html)
     }
 }
